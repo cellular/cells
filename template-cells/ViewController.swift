7e14ad4ff13ca8a17467d99fc470a5bdf15f40e9
Dimitri Brukakis <dbrukakis@cellular.de>
2020-05-11 22:53:08 +0200

    Initial Commit

---
+++ template-cells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  template-cells
+//
+//  Created by Dimitrios Brukakis on 11.05.20.
+//  Copyright © 2020 Cellular. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ template-cellsTests/template_cellsTests.swift
@@ -0,0 +1,34 @@
+//
+//  template_cellsTests.swift
+//  template-cellsTests
+//
+//  Created by Dimitrios Brukakis on 11.05.20.
+//  Copyright © 2020 Cellular. All rights reserved.
+//
+
+import XCTest
+@testable import template_cells
+
+class template_cellsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
